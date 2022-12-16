2215d4f92c68e78721de0cd8e59a4f06f1505aa7
devin-lynch <devin.l.lynch@gmail.com>
2022-12-16 17:56:24 -0600

    Component Selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  selector: 'app-servers', // generally use this one for components!
   template:  // ALWAYS need a template/templateUrl component! 
-    `<app-server></app-server>
+    `<app-server></app-server> 
     <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
