c383100b39dcba38497b085dccaa8679b0fd508c
devin-lynch <devin.l.lynch@gmail.com>
2022-12-17 01:12:32 -0600

    Property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,11 +4,17 @@ import { Component } from '@angular/core';
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers', // generally use this one for components!
-  template:  // ALWAYS need a template/templateUrl component! 
-    `<app-server></app-server> 
-    <app-server></app-server>`,
+  // template:  // ALWAYS need a template/templateUrl component! 
+  //   `<app-server></app-server> serv
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false
 
+  serverDelay = setTimeout(() => {
+    this.allowNewServer = true;
+  }, 2000)
+ 
 }
